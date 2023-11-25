6dd482f1273ec89c3c678a56021bae99e7440c5b
s-ko <song.1209@icloud.com>
2023-11-25 15:30:57 +0900

    chore: setup displays templates/mysite/index.html

--- mysite/views.py
+++ mysite/views.py
@@ -3,4 +3,4 @@ from django.http import HttpResponse
 
 # Create your views here.
 def index(request):
-  return HttpResponse('Hello Django brog site')
+    return render(request, 'mysite/index.html', {})
