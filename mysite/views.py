a265816da3847139162d51e06557f8a45d6e5d60
s-ko <song.1209@icloud.com>
2023-11-29 22:37:40 +0900

    set: articles for views.py, displays

--- mysite/views.py
+++ mysite/views.py
@@ -1,9 +1,12 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from blog.models import Article
 
 # Create your views here.
 def index(request):
+    articles = Article.objects.all()
     context = {
-        'title': 'Django Brog Site'
+        'title': 'Django Brog Site',
+        'articles': articles,
 	}
     return render(request, 'mysite/index.html', context)
