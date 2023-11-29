64fec49e50f5ef1ec611507e992bc99872a7bf02
s-ko <song.1209@icloud.com>
2023-11-29 22:53:59 +0900

    add: displays article

--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,12 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from blog.models import Article
 
 # Create your views here.
-def test(request):
-    return HttpResponse('<h1>Hello</h1>')
+def article(request, pk):
+    article = Article.objects.get(id=pk)
+    context = {
+        'article': article
+    }
+
+    return render(request, 'blog/article.html', context)
