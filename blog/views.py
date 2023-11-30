c0dc2c18e52268eeeb3cdb468c1eea5d5c9d87e1
s-ko <song.1209@icloud.com>
2023-11-30 22:49:22 +0900

    add: blogs index page

--- blog/views.py
+++ blog/views.py
@@ -2,11 +2,16 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from blog.models import Article
 
-# Create your views here.
+def index(request):
+    articles = Article.objects.all()
+    context = {
+        'articles': articles,
+    }
+    return render(request, 'blog/blogs.html', context)
+
 def article(request, pk):
     article = Article.objects.get(id=pk)
     context = {
         'article': article
     }
-
     return render(request, 'blog/article.html', context)
