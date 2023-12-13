4d35ce217e995b81ab010dde957638c2f594da1c
s-ko <song.1209@icloud.com>
2023-12-14 00:05:04 +0900

    fix: typo

--- mysite/models/profile_models.py
+++ mysite/models/profile_models.py
@@ -1,8 +1,8 @@
 from django.db import models
-from django.contrib import get_user_model
+from django.contrib.auth import get_user_model
 
 class Profile(models.Model):
-    user       = models.OneToOneField(get_user_model(), unique=True, on_delete=models.CASCADE, primary_key=True)
+	user       = models.OneToOneField(get_user_model(), unique=True, on_delete=models.CASCADE, primary_key=True)
 	username   = models.CharField(default="匿名ユーザー", max_length=30)
 	zipcode    = models.CharField(default="", max_length=8)
 	prefecture = models.CharField(default="", max_length=6)
