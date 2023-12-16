bed2475bf446f2b631ce3031edb4010ed3e5aff2
s-ko <song.1209@icloud.com>
2023-12-16 12:42:58 +0900

    add: profile inline displays

--- mysite/admin.py
+++ mysite/admin.py
@@ -5,6 +5,10 @@ from mysite.models.account_models import User
 from mysite.models.profile_models import Profile
 from mysite.forms import UserCreationForm
 
+class ProfileInline(admin.StackedInline):
+    model = Profile
+    can_delete = False
+
 class CustomUserAdmin(UserAdmin):
     fieldsets = (
         (None, {
@@ -31,7 +35,8 @@ class CustomUserAdmin(UserAdmin):
         }),
     )
     add_form = UserCreationForm  # adminでuser作成用
+    inlines = (ProfileInline,)  # ProfileモデルはUserモデルとOneToOneで紐づいているので、User情報をみる際に一緒に表示するために追記
 
 admin.site.unregister(Group)
 admin.site.register(User, CustomUserAdmin)
-admin.site.register(Profile)
+# admin.site.register(Profile) # Profileの項目表示
