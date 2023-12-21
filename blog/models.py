109edb2331ad472eb6664d7da03368bb31225fad
s-ko <song.1209@icloud.com>
2023-12-21 22:50:51 +0900

    add: count

--- blog/models.py
+++ blog/models.py
@@ -5,6 +5,7 @@ class Article(models.Model):
     title      = models.CharField(default="", max_length=30)
     text       = models.TextField(default="")
     author     = models.CharField(default="", max_length=30)
+    count      = models.IntegerField(default=0)
     created_at = models.DateField(auto_now_add=True)
     updated_at = models.DateField(auto_now=True)
 
