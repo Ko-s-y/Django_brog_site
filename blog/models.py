b667ce4fe041ef7f6c7ba5e73151ec411c4dfea7
s-ko <song.1209@icloud.com>
2023-12-24 22:59:47 +0900

    add: tag model

--- blog/models.py
+++ blog/models.py
@@ -1,11 +1,19 @@
 from django.db import models
 from django.contrib.auth import get_user_model
 
+class Tag(models.Model):
+    slug = models.CharField(primary_key=True, unique=True, max_length=20)
+    name = models.CharField(unique=True, max_length=20)
+
+    def __str__(self):
+        return self.slug
+
 class Article(models.Model):
     title      = models.CharField(default="", max_length=30)
     text       = models.TextField(default="")
     author     = models.CharField(default="", max_length=30)
     count      = models.IntegerField(default=0)
+    tags       = models.ManyToManyField(Tag, blank=True)
     created_at = models.DateField(auto_now_add=True)
     updated_at = models.DateField(auto_now=True)
 
