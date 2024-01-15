91bcdccd31bd7a0a8fc6230df6faeb2cce43e697
s-ko <song.1209@icloud.com>
2024-01-15 23:12:00 +0900

    chore: setup gmail with yaml environ

--- config/settings.py
+++ config/settings.py
@@ -156,3 +156,12 @@ MESSAGE_TAGS = {
     messages.DEBUG: 'rounded-0 alert alert-secondary',
 }
 # --------- massage tab with bootstrap alert class ----------------
+
+# --- Gmail 送信設定 ---
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_HOST_USER = os.environ['EMAIL_HOST_USER']
+EMAIL_HOST_PASSWORD = os.environ['EMAIL_HOST_PASSWORD']
+# --- Gmail 送信設定 ---
